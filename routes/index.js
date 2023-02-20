cd3c64eaa39f142b75d4a72a98d68c9269619ba0
Tina Yang <Tinayangers@gmail.com>
2023-02-20 11:23:10 -0500

    Added testing route

---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const { Router } = require('express');
+const controllers = require('../controllers')
+const router = Router();
+
+router.get('/', (req, res) => res.send('This is root!'))
+
+module.exports = router;
