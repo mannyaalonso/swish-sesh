9fd545f77c742a01839fb025959effcfd72cd9cb
Tina Yang <Tinayangers@gmail.com>
2023-02-20 10:54:24 -0500

    Added models and db

---
+++ models/runs.js
@@ -0,0 +1,15 @@
+const { Schema } = require('mongoose')
+
+const runsSchema = new Schema(
+    {
+        date: { type: String, required: true },
+        time: { type: String, required: true },
+        host: { type: String, required: true },
+        players: { type: Schema.Types.ObjectId, ref: 'User' },
+        location: { type: string, required: true },
+        isFull: { type: Boolean, required: true }
+    },
+    { timestamps: true }
+)
+
+module.exports = runsSchema
