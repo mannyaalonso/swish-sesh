23d65af502e36dd5875b6c0360fb77cd6c7f2dc8
Freddy Caione <fac299@nyu.edu>
2023-02-20 16:33:43 -0500

    fix home rendering of runs

--- routes/index.js
+++ routes/index.js
@@ -10,11 +10,8 @@ router.get('/user/:id', controllers.getUserById)
 router.put('/user/:id', controllers.updateUser)
 router.delete('/user/:id', controllers.deleteUser)
 
-
-
-
 router.post('/run', controllers.createRun)
-router.get('/runs', controllers.getAllRuns)
+// router.get('/runs', controllers.getAllRuns)
 router.get('/run/:id', controllers.getRunById)
 router.put('/run/:id', controllers.updateRun)
 router.delete('/run/:id', controllers.deleteRun)
