22a22537b18411897605287907475e91643f91b8
Tina Yang <Tinayangers@gmail.com>
2023-02-20 16:35:20 -0500

    Updated

--- controllers/index.js
+++ controllers/index.js
@@ -82,16 +82,7 @@ const getAllRuns = async (req, res) => {
     try {
         const { id } = req.params
         const runs = await Run.find({ players: id })
-        return res.status(200).json({runs})
-    } catch (error) {
-        return res.status(500).send(error.message);
-    }
-}
-
-const getRuns = async (req, res) => {
-    try {
-        const runs = await Run.find()
-        return res.status(200).json(runs)
+        return res.status(200).json({ runs })
     } catch (error) {
         return res.status(500).send(error.message);
     }
