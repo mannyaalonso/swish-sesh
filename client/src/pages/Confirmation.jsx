86388b9714e05c751de1f9471f2e25f88464a058
Hyun Bin Yim <hby0818@gmail.com>
2023-02-20 11:23:01 -0500

    Confirmtationwords

--- client/src/pages/Confirmation.jsx
+++ client/src/pages/Confirmation.jsx
@@ -3,7 +3,9 @@
 const Confirmation = () => {
   return (
     <div>
-      
+      <h1>Welcome to the SwishSesh Community!
+        <p>Check if we have available spots and secure your spot now!</p>
+      </h1>
     </div>
   )
 }
