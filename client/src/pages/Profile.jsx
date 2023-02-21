3c17346ca41d98714514afdbe5905b90dd3642ec
Tina Yang <Tinayangers@gmail.com>
2023-02-20 20:45:58 -0500

    Updated Profile

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,42 +1,65 @@
 import axios from 'axios'
 import { useEffect, useState, } from 'react'
 
-const Profile = ({ user }) => {
-  //const [user, setUser] = useState(user)
-  const [updateExp, setUpdateExp] = useState({
-    experience: `${user.experience}`,
+const Profile = (
+  // { user }
+) => {
+  // const [currentUser, setCurrentUser] = useState(user)
+  const [currentUser, setCurrentUser] = useState('')
+  const [updateUser, setUpdateUser] = useState({
+    experience: `${currentUser.experience}`,
   })
+  const [editing, setEditing] = useState(false)
+  // const [currentUserId, setCurrentUserId] = useState('')
 
-  console.log(user.experience)
+  console.log(currentUser.experience)
 
   // const getUser = async (e) => {
   //   try {
   //     const res = await axios.get(`/api/user/${user._id}`)
-  //     // setUser(res.data.user)
-  //     setUser(res)
+  //     setCurrentUser(res.data.user)
+  //     // setCurrentUser(res)
   //     // console.log(res)
   //   } catch (err) {
   //     console.log(err)
   //   }
   // }
 
+  // useEffect(() => {
+  //   getUser()
+  // }, [])
+
+  const getUser = async (e) => {
+    try {
+      const res = await axios.get(`/api/user/63f3f2c0f1105e076a869468`)
+      setCurrentUser(res.data.user)
+      // setCurrentUser(res)
+      // console.log(res.data.user)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   useEffect(() => {
-    //getUser()
+    getUser()
   }, [])
 
   const handleSubmit = async (e) => {
     // console.log(e)
     e.preventDefault()
     const updateExpPackage = {
-      experience: updateExp.experience,
+      experience: updateUser.experience,
     }
-    const response = await axios.put(`/api/user/63f3d9d269dd3b32ef27e0f3`, updateExpPackage)
-    console.log(updateExp)
+    console.log(updateExpPackage)
+    const response = await axios.put(`/api/user/63f3f2c0f1105e076a869468/`, updateExpPackage)
   }
-
+  // const handleEditState = (currentUser) => {
+  //   setUpdateExp({ experience: `${currentUser.experience}` })
+  //   setUserId(currentUser._id)
+  // }
   const handleChange = (e) => {
     // e.preventDefault()
-    setUpdateExp({
+    setUpdateUser({
       // ...updateExp,
       [e.target.name]: e.target.value,
     })
@@ -94,7 +117,7 @@ const Profile = ({ user }) => {
                   htmlFor="first-name"
                   className="block text-sm font-medium text-gray-700"
                 >
-                  Full Name: {user.name}
+                  Full Name: {currentUser.name}
                 </label>
               </div>
 
@@ -103,42 +126,69 @@ const Profile = ({ user }) => {
                   htmlFor="email"
                   className="block text-sm font-medium text-gray-700"
                 >
-                  Email address: {user.email}
+                  Email address: {currentUser.email}
                 </label>
               </div>
+
 
               <div className="sm:col-span-4">
                 <label
                   htmlFor="experience"
                   className="block text-sm font-medium text-gray-700"
                 >
-                  Experience
+                  Experience: {currentUser.experience}
                 </label>
                 <div className="mt-1">
-                  <select
-                    id="experience"
-                    name="experience"
-                    type="text"
-                    // autoComplete="experience"
-                    className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                    onChange={handleChange}
+                  <button
+                    className="ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    onClick={(e) => { e.preventDefault(); setEditing(!editing) }}
                   >
-                    <option value="Recreational & Friendly">
-                      Recreational & Friendly
-                    </option>
-                    <option value="Competitive">Competitive</option>
-                    <option value="Professional">Professional</option>
-                    <option value="Elite">Elite</option>
-                  </select>
+                    Edit
+                  </button>
                 </div>
               </div>
+
+              {editing &&
+                <div className="sm:col-span-4">
+                  <label
+                    htmlFor="updateExp"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Experience
+                  </label>
+                  <div className="mt-1">
+                    <select
+                      id="updateExp"
+                      name="experience"
+                      type="text"
+                      // autoComplete="experience"
+                      className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                      onChange={handleChange}
+                      value={updateUser.experience}
+                    >
+                      <option value="Recreational & Friendly">
+                        Recreational & Friendly
+                      </option>
+                      <option value="Competitive">Competitive</option>
+                      <option value="Professional">Professional</option>
+                      <option value="Elite">Elite</option>
+                    </select>
+                    <button
+                      type="submit"
+                      className="ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                      onSubmit={handleSubmit}
+                    >
+                      Update
+                    </button>
+                  </div>
+                </div>}
 
               <div className="sm:col-span-4">
                 <label
                   htmlFor="pastRun"
                   className="block text-sm font-medium text-gray-700"
                 >
-                  Past Runs: {user.pastRuns}
+                  Past Runs: {currentUser.pastRuns}
                 </label>
                 <div className="mt-1"></div>
               </div>
@@ -146,7 +196,7 @@ const Profile = ({ user }) => {
           </div>
         </div>
 
-        <div className="pt-5">
+        {/* <div className="pt-5">
           <div className="flex justify-end">
             <button
               type="button"
@@ -162,7 +212,7 @@ const Profile = ({ user }) => {
               Save
             </button>
           </div>
-        </div>
+        </div> */}
       </form>
     </div>
   )
