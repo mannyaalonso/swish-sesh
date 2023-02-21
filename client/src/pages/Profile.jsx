1adf7d102fd8d8474623a9a07db7674477acdfdb
Tina Yang <Tinayangers@gmail.com>
2023-02-20 22:03:25 -0500

    Updated

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -162,7 +162,6 @@ const Profile = (
                       id="updateExp"
                       name="experience"
                       type="text"
-                      // autoComplete="experience"
                       className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                       onChange={handleChange}
                       value={updateUser}
