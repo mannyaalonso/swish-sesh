b9d28ecf931bf1a23ffefc4dd0010009ca2b6aae
Tina Yang <Tinayangers@gmail.com>
2023-04-15 14:19:52 -0400

    reminder to add sorting with arrow

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import axios from "axios"
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import Footer from "../components/Footer"
-
+//Need to add sorting with arrow
 const Profile = ({ userId }) => {
   let navigate = useNavigate()
   const [currentUser, setCurrentUser] = useState("")
